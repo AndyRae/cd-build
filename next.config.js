2f93fe9049f9f2d3190d87b804826e6d42ef45ca
Andy Rae <1127507+AndyRae@users.noreply.github.com>
2026-02-27 08:19:20 +0000

    Why

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 import createMDX from "@next/mdx";
 import path from "path";
+import { fileURLToPath } from "url";
 /** @type {import('next').NextConfig} */
 
+// __dirname is not available in native ES modules, so we recreate it.
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const nextConfig = {
   output: "standalone",
