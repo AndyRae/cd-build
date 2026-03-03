223a28f89ec278cb7249ecd3cdd6706d7353a882
Andy Rae <1127507+AndyRae@users.noreply.github.com>
2026-03-03 20:45:10 +0000

    Fixes

--- next.config.js
+++ next.config.js
@@ -1,11 +1,5 @@
 import createMDX from "@next/mdx";
-import path from "path";
-import { fileURLToPath } from "url";
 /** @type {import('next').NextConfig} */
-
-// __dirname is not available in native ES modules, so we recreate it.
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 const nextConfig = {
   output: "standalone",
@@ -18,14 +12,6 @@ const nextConfig = {
     authInterrupts: true,
   },
   transpilePackages: ["@faker-js/faker"],
-  outputFileTracingRoot: path.join(__dirname, "../"),
-  // Optimize build performance
-  swcMinify: true,
-  // Enable build caching
-  // onDemandEntries: {
-  //   maxInactiveAge: 25 * 1000,
-  //   pagesBufferLength: 2,
-  // },
 };
 
 const withMDX = createMDX({
